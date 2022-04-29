5a6d67d8d5f1b8e2f72004065d76e3c509a4c51f
Elliot Soloway <soloway@umich.edu>
2022-04-29 16:09:45 -0400

    Update links.js

--- links.js
+++ links.js
@@ -29,7 +29,7 @@ var links = {
     "Team Evaluation Section 1": "https://docs.google.com/forms/d/e/1FAIpQLScM6wwBwufLgvMpfxxMGXg2eJcwmxpVZs1nuql25Tj_WGooug/viewform?usp=sf_link",
     "Team Evaluation Section 2": "https://docs.google.com/forms/d/e/1FAIpQLSeVqQoWEyujfamkVAiE8rtOOVfolAO9J4fBlj-UPolxA9j9Jg/viewform?usp=sf_link",
 
-    "Elevator Pitch Due Date": "",
+    "Problem Pitch Due Date": "",
 
     "Assignment 0 Due Date": "",
     "Assignment 0 Folder Section 1": "https://drive.google.com/drive/u/2/folders/1vwAb1uuIqa45wMEgY6Se0sEmnU5FNGTv",
